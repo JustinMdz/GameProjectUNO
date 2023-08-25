9ecd736c016a5de76fd0653ad5519d3803fb4ba2
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-08-25 08:35:04 -0600

    New base functions in the DeckCardList class

--- Project UNO/Project UNO/DeckCardList.cpp
+++ Project UNO/Project UNO/DeckCardList.cpp
@@ -1,1 +1,63 @@
 #include "DeckCardList.h"
+
+DeckCardList::DeckCardList()
+{
+	firstNode = currentNode = NULL;
+}
+
+DeckCardList::~DeckCardList()
+{
+	while (firstNode != NULL) {
+		currentNode = firstNode;
+		firstNode = firstNode->getNextNode();
+		delete currentNode;
+	}
+}
+
+void DeckCardList::insertFirstNode(Card* ptrCard)
+{
+	firstNode = new Node(ptrCard, firstNode);
+}
+
+void DeckCardList::deleteFirstNode()
+{
+	currentNode = firstNode;
+	if (firstNode != NULL) {
+		firstNode = currentNode->getNextNode();
+		delete currentNode;
+	}
+}
+
+void DeckCardList::insertLastNode(Card* ptrCard)
+{
+	if (firstNode == NULL) {
+		firstNode = new Node(ptrCard, firstNode);
+	}
+	else {
+		currentNode = firstNode;
+		while (currentNode->getNextNode() != NULL) {
+			currentNode = currentNode->getNextNode();
+		}
+		currentNode->setNextNode(new Node(ptrCard, NULL));
+	}
+}
+
+void DeckCardList::deleteLastNode()
+{
+	currentNode = firstNode;
+	Node* lastNode = currentNode;
+	if (firstNode != NULL) {
+		if (firstNode->getNextNode() == NULL) {
+			delete firstNode;
+			firstNode = NULL;
+		}
+		else {
+			while (currentNode->getNextNode() != NULL) {
+				lastNode = currentNode;
+				currentNode = currentNode->getNextNode();
+			}
+			lastNode->setNextNode(NULL);
+			delete  currentNode;
+		}
+	}
+}
--- Project UNO/Project UNO/DeckCardList.h
+++ Project UNO/Project UNO/DeckCardList.h
@@ -1,9 +1,18 @@
 #pragma once
 #include "Librarys.h"
+#include "Node.h"
 class DeckCardList
 {
 private:
-
+	Node* firstNode;
+	Node* currentNode;
 public:
+	DeckCardList();
+	~DeckCardList();
+
+	void insertFirstNode(Card* ptrCard);
+	void deleteFirstNode();
+	void insertLastNode(Card* ptrCard);
+	void deleteLastNode();
 };
 
