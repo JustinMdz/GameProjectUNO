16709bb756a444f3fa2725fa9473005ec3c8e2de
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-09-13 14:30:38 -0600

    added new functions in Game Class, startGame, fill players names and principal while in game

--- Project UNO/Project UNO/Game.cpp
+++ Project UNO/Project UNO/Game.cpp
@@ -2,4 +2,46 @@
 
 Game::Game()
 {
+    playerOne = new Player();
+    playerTwo = new Player();
+    gameCardDeckList = new DeckCardList();
+    gameDiscardDeckList = new DeckCardList();
+    gameDealer = new Dealer();
+}
+
+void Game::fillPlayerName()
+{
+    string playerName;
+
+    cout << "Enter a name for playerOne: ";
+    cin >> playerName;
+    playerOne->setName(playerName);
+
+    cout << "\n\nEnter a name for playerTwo: ";
+    cin >> playerName;
+    playerTwo->setName(playerName);
+
+}
+
+void Game::start()
+{
+    bool endOfTheGame = false;
+
+    fillPlayerName();
+
+    while (endOfTheGame != true) {
+
+        cout << "Game started";
+    }
+}
+
+bool Game::isGameFinished(Player*& playerOnTurn)
+{
+    bool gameFinished = false;
+
+    if (playerOnTurn->getPlayerDeck()->isListEmpty()) {
+        gameFinished = true;
+    }
+
+    return gameFinished;
 }
--- Project UNO/Project UNO/Game.h
+++ Project UNO/Project UNO/Game.h
@@ -1,10 +1,21 @@
 #pragma once
 #include "Librarys.h"
+#include "Player.h"
+#include "Dealer.h"
+
 class Game
 {
 private:
+	Player* playerOne;
+	Player* playerTwo;
+	DeckCardList* gameCardDeckList;
+	DeckCardList* gameDiscardDeckList;
+	Dealer* gameDealer;
 
 public:
 	Game();
+	void fillPlayerName();
+	void start();
+	bool isGameFinished(Player*&);
 	~Game();
 };
