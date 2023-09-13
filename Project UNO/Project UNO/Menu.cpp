1a9bead6cac637bd39cb6d07ff4f686902a5be3c
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-09-13 14:11:04 -0600

    added print functions in Menu Class

--- Project UNO/Project UNO/Menu.cpp
+++ Project UNO/Project UNO/Menu.cpp
@@ -1,1 +1,46 @@
 #include "Menu.h"
+
+Menu::Menu(float width, float height)
+{
+	if (!fontType.loadFromFile("Fonts/arial.ttf")) {
+		cout << "No font is here";
+	}
+
+	int yPosIndex = 0;
+	for (int index = 0; index < MENU_OPTIONS; index++) {
+		yPosIndex += 200;
+		mainMenuOptions[index].setFont(fontType);
+		mainMenuOptions[index].setFillColor(Color::White);
+		mainMenuOptions[index].setCharacterSize(70);
+		mainMenuOptions[index].setPosition(400, yPosIndex);
+	}
+	mainMenuOptions[0].setString("Play");
+	mainMenuOptions[1].setString("Options");
+	mainMenuOptions[2].setString("Exit");
+
+	selectedOptionMenuIndex = -1;
+}
+
+int Menu::getPressedItem()
+{
+	return selectedOptionMenuIndex;
+}
+
+bool Menu::getOption()
+{
+	return inOption;
+}
+
+void Menu::isPosibleToDraw(RenderWindow& menuBackgroundWindow)
+{
+	if (inOption == true) {
+		drawMenu(menuBackgroundWindow);
+	}
+}
+
+void Menu::drawMenu(RenderWindow& menuBackgroundWindow)
+{
+	for (int index = 0; index < MENU_OPTIONS; index++) {
+		menuBackgroundWindow.draw(mainMenuOptions[index]);
+	}
+}
--- Project UNO/Project UNO/Menu.h
+++ Project UNO/Project UNO/Menu.h
@@ -2,11 +2,25 @@
 #include "Librarys.h"
 #include <SFML/Graphics.hpp>
 using namespace sf;
+const int static MENU_OPTIONS = 3;
 
 class Menu
 {
 private:
+	Font fontType;
+	Text mainMenuOptions[MENU_OPTIONS];
+	int selectedOptionMenuIndex;
+	bool inOption = true;
 
 public:
+	Menu(float, float);
+
+	int getPressedItem();
+	bool getOption();
+
+	void isPosibleToDraw(RenderWindow& menuBackgroundWindow);
+	void drawMenu(RenderWindow&);
+
+	~Menu();
 };
 
