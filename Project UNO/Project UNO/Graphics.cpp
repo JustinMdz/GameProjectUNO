c642320ef82832d1692d4b22a77d17ae0a3e8234
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-09-13 14:48:46 -0600

    added functions to load cards in the Graphics class

--- Project UNO/Project UNO/Graphics.cpp
+++ Project UNO/Project UNO/Graphics.cpp
@@ -1,6 +1,6 @@
 #include "Graphics.h"
 
-Graphics::Graphics() : backgroundWindow(VideoMode(1080, 720), "SFML Image Upload") {
+Graphics::Graphics() : backgroundWindow(VideoMode(1080, 720), "BackWindow") {
     if (!backgroundTexture.loadFromFile("gameBackground.jpg")) {
         return;
     }
@@ -11,10 +11,17 @@ void Graphics::processEvents()
 {
     Event event;
     while (backgroundWindow.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
+        if (event.type == Event::Closed) {
             backgroundWindow.close();
         }
+      
     }
+}
+
+void Graphics::render() {
+    backgroundWindow.clear();
+    backgroundWindow.draw(backgroundSprite);
+    backgroundWindow.display();
 }
 
 void Graphics::extendBackground()
@@ -26,16 +33,30 @@ void Graphics::extendBackground()
     );
 }
 
-void Graphics::render() {
-    backgroundWindow.clear();
-    backgroundWindow.draw(backgroundSprite);
-    backgroundWindow.display();
-}
-
 void Graphics::runBackground() {
+
     while (backgroundWindow.isOpen()) {
-        processEvents();
         extendBackground();
         render();
+        processEvents();
     }
+}
+
+void Graphics::loadCardTexture(string cardId, string cardColor)
+{
+    if (!cardTexture.loadFromFile(cardId + cardColor + ".jpg")) {
+        cout << "Load card texture error";
+    }
+}
+
+void Graphics::loadBackCardTexture()
+{
+    if (!cardTexture.loadFromFile("Back.jpg")) {
+        cout << "Load card texture error";
+    }
+}
+
+void Graphics::createCardSprite(Texture& textureOfCard)
+{
+    cardSprite.setTexture(textureOfCard);
 }
--- Project UNO/Project UNO/Graphics.h
+++ Project UNO/Project UNO/Graphics.h
@@ -1,20 +1,31 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "Librarys.h"
+#include "Menu.h"
+#include "Button.h"
+#include "DeckCardList.h"
 using namespace sf;
 
 class Graphics
 {
-private: 
+private:
+
 	RenderWindow backgroundWindow;
 	Texture backgroundTexture;
 	Sprite backgroundSprite;
 
+	Texture cardTexture;
+	Sprite cardSprite;
+
+	
+public:
+	Graphics();
+
 	void processEvents();
 	void render();
 	void extendBackground();
-
-public:
-	Graphics();
 	void runBackground();
+
+	void loadCardTexture(string, string);
+	void loadBackCardTexture();
+	void createCardSprite(Texture&);
 };
