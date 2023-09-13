227a0c34dd120b4b73ad71544c55a4ce97c71b38
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-09-13 14:04:37 -0600

    add empty Menu Class

---
+++ Project UNO/Project UNO/Menu.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Librarys.h"
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Menu
+{
+private:
+
+public:
+};
+
--- Project UNO/Project UNO/Node.h
+++ Project UNO/Project UNO/Node.h
@@ -16,4 +16,3 @@ public:
 	Node* getNextNode();
 	string toString();
 };
-
