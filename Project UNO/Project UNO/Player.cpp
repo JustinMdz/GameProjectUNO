36adb661940b12647776f70e9407414117101b49
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-09-13 13:42:47 -0600

    add constructors, set and gets and printPlayerDeck in player class

--- Project UNO/Project UNO/Player.cpp
+++ Project UNO/Project UNO/Player.cpp
@@ -1,1 +1,34 @@
 #include "Player.h"
+
+Player::Player()
+{
+	playerDeckCardList = new DeckCardList();
+}
+
+void Player::setName(string nameOfPlayer)
+{
+	this->playerName = nameOfPlayer;
+
+}
+
+string Player::getName()
+{
+	return playerName;
+}
+
+void Player::setPlayerDeck(DeckCardList* deckOfPlayer)
+{
+	this->playerDeckCardList = deckOfPlayer;
+}
+
+DeckCardList* Player::getPlayerDeck()
+{
+	return playerDeckCardList;
+}
+
+void Player::printPlayerDeck()
+{
+	playerDeckCardList->printList();
+}
+
+Player::~Player() {}
--- Project UNO/Project UNO/Player.h
+++ Project UNO/Project UNO/Player.h
@@ -1,6 +1,24 @@
 #pragma once
 #include "Librarys.h"
+#include "DeckCardList.h"
+#include "Node.h"
 class Player
 {
-};
+private:
+	string playerName;
+	DeckCardList* playerDeckCardList;
 
+public:
+
+	Player();
+
+	void setName(string);
+	string getName();
+
+	void setPlayerDeck(DeckCardList* deckOfPlayer);
+	DeckCardList* getPlayerDeck();
+
+	void printPlayerDeck();
+
+	~Player();
+};
