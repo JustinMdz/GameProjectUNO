fbbe8734ff0c2ab40cb14d3fb11356bb2928a6c2
MdzJustin <justin.mendez.mena@est.una.ac.cr>
2023-09-13 13:53:27 -0600

    Added functions in Player class, grabCard, checkPlayerWrirttenCard and selectcCard to Trow

--- Project UNO/Project UNO/Player.h
+++ Project UNO/Project UNO/Player.h
@@ -22,6 +22,15 @@ public:
 	bool checkIsPossibleTrowSpecialCard();
 	bool checkIsTrowPossible(DeckCardList*&);
 
+	bool checkPlayerWrittenCard(string, string, DeckCardList*&);
+	void selectCardToTrow(DeckCardList*&);
+
+	void trowNormalCard(DeckCardList*& discardMaze);
+	void trowSpecialCard();
+	void trowCard();
+
+	void grabCard(int cardsToDraw, DeckCardList*& deckOfGame);
+
 	void printPlayerDeck();
 
 	~Player();
